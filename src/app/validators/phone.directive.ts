0e1f75dad8e2b93cdf11518f66392e844a6d77b7
Diego Cantelli <diegocantelli19@gmail.com>
2020-12-31 19:29:25 -0300

    Criando um validador de formulário customizado

--- src/app/validators/phone.directive.ts
+++ src/app/validators/phone.directive.ts
@@ -3,13 +3,17 @@ import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 import { PhoneValidator } from './phone.validator';
 
 @Directive({
-    selector: '[phone][ngModel]',
-    providers: [{ provide: NG_VALIDATORS, useExisting: PhoneDirective, multi: true }]
+  selector: '[phone][ngModel]',
+
+  //definindo esta diretiva como parte da lista de validadores do Angular
+  providers: [{ provide: NG_VALIDATORS, useExisting: PhoneDirective, multi: true }]
 })
+
+//Necessário implementar a interface Validator
 export class PhoneDirective implements Validator {
   private validator = PhoneValidator();
 
   validate(control: AbstractControl): { [key: string]: any } {
-      return this.validator(control);
+    return this.validator(control);
   }
 }
--- src/app/validators/phone.validator.ts
+++ src/app/validators/phone.validator.ts
@@ -1,10 +1,16 @@
+//Imports necessários para se criar um validador de formulário customizado
+
+//Para usar esse validador, é necessário criar uma diretiva
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 const expression = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/;
 
+//Deve ser exportada uma função que retorne o tipo ValidatorFn
 export function PhoneValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } => {
     const valid = expression.test(control.value) && control.value.length < 14;
+
+    //Caso seja nulo, significa que o valor é válido, caso contrário, é inválido
     return valid ? null : { phone: true };
   };
 }
